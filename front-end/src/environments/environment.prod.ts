5cd672560f607f61d0b3d846d9c3a54a1db0fc44
MAnimuonyam <139596610+MAnimuonyam@users.noreply.github.com>
2023-12-22 21:17:04 -0500

    Update environment.prod.ts

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  resourceApi: "https://api.mariomerco.com/resources-api",
-  inventoryApi: "https://api.mariomerco.com/inventory-api",
-  clientApi: "https://api.mariomerco.com/clients-api",
-  rentingApi: "https://api.mariomerco.com/renting-api" 
+  resourceApi: "https://api.banifami.com/resources-api",
+  inventoryApi: "https://api.banifami.com/inventory-api",
+  clientApi: "https://api.banifami.com/clients-api",
+  rentingApi: "https://banifami.com/renting-api" 
 };
