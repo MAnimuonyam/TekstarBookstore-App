f5464994a97bd3b021827731105b034b787d9f20
MAnimuonyam <139596610+MAnimuonyam@users.noreply.github.com>
2023-12-22 21:18:42 -0500

    Update environment.prod.ts

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   resourceApi: "https://api.banifami.com/resources-api",
   inventoryApi: "https://api.banifami.com/inventory-api",
   clientApi: "https://api.banifami.com/clients-api",
-  rentingApi: "https://banifami.com/renting-api" 
+  rentingApi: "https://api.banifami.com/renting-api" 
 };
